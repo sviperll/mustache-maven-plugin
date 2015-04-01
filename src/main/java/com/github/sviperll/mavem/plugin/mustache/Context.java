3d8ad666bcc9623ce537374b6c196f8a9813948a
Victor Nazarov <asviraspossible@gmail.com>
2015-04-01 14:00:49 +0300

    Initial commit

---
+++ src/main/java/com/github/sviperll/mavem/plugin/mustache/Context.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright 2015 Victor Nazarov <asviraspossible@gmail.com>.
+ */
+package com.github.sviperll.mavem.plugin.mustache;
+
+import java.io.File;
+import java.util.List;
+
+/**
+ *
+ * @author Victor Nazarov <asviraspossible@gmail.com>
+ */
+public class Context {
+    /**
+     * File used as a context for mustache templates.
+     */
+    File file;
+
+    /**
+     * Type of context file.
+     *
+     * <ul>
+     *   <li>json
+     *   <li>properties
+     * </ul>
+     */
+    String type;
+
+    /**
+     * File to read or write properties to.
+     */
+    List<Template> templates;
+
+}
